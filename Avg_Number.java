058fc1f99c4dc364aebd4bb55cf6e33c4a403a8a
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-08-20 22:44:22 +0530

    Add files via upload

---
+++ Avg_Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Avg_Number {
+	
+	
+	    public static void main(String[] args)
+	    {
+	        Scanner s = new Scanner(System.in);
+	        double a = s.nextDouble();
+	        double b = s.nextDouble();
+	        double avg = 0;
+	        avg = (a+b)/2;
+	        System.out.format("%.5f",+avg);
+	    }
+	}
